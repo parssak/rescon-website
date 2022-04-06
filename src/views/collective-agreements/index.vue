974b835665b667f557782f8768e522aa11c0e459
parssak <parssak@gmail.com>
2022-04-06 07:12:34 -0400

    basic contact page done

--- src/views/collective-agreements/index.vue
+++ src/views/collective-agreements/index.vue
@@ -1,20 +1,91 @@
 <template>
-  <Header title='collective-agreements' />
-  
-  
+  <Header title="collective agreements" />
+
+  <Container>
+    <Menu>
+      <div class="relative w-max">
+        <span class="flex rounded-md uppercase bg-gray-100 w-max">
+          <span class="px-4 py-2"> Select time period </span>
+          <MenuButton class="bg-accent text-white px-4 py-2 rounded-r-md flex items-center">
+            {{ selected }}
+            <img src="@/assets/down.svg" alt="" class="w-3 h-3 ml-2" />
+          </MenuButton>
+        </span>
+        <MenuItems
+          class="absolute top-full right-0 grid border mt-2 rounded-md bg-white shadow-sm border"
+        >
+          <MenuItem v-slot="{ active }" v-for="option in options" :key="option">
+            <button
+              :class="`px-2 py-1 ${active ? 'bg-accent-lightest' : ''}`"
+              @click="() => setSelected(option)"
+            >
+              {{ option }}
+            </button>
+          </MenuItem>
+        </MenuItems>
+      </div>
+    </Menu>
+
+    <h2 class="mt-12">May 1, 2019 - April 30, 2022</h2>
+    <div class="grid gap-6">
+      <a
+        v-for="(newsletter, index) in newsletters"
+        :key="index"
+        class="flex items-center space-x-2"
+        href="#"
+      >
+        <DownloadIcon class="w-7 h-7 text-accent" />
+        <p class="text-accent underline">{{ newsletter }} {{ selected }}</p>
+      </a>
+    </div>
+  </Container>
 </template>
 
 <script>
+import Container from "../../components/layouts/Container.vue";
 import Header from "@/components/base/Header";
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import { DownloadIcon } from "@heroicons/vue/outline";
 
-
-    
-
+const options = ["2017", "2018", "2019", "2020", "2021", "2022"];
+const newsletters = [
+  "DRCLB Collective Agreement",
+  "MTABA Collective Agreement",
+  "TRCLB Collective Agreement",
+  "DRCLB Collective Agreement",
+  "MTABA Collective Agreement",
+  "TRCLB Collective Agreement",
+  "DRCLB Collective Agreement",
+  "MTABA Collective Agreement",
+  "TRCLB Collective Agreement",
+  "DRCLB Collective Agreement",
+  "MTABA Collective Agreement",
+  "TRCLB Collective Agreement",
+  "DRCLB Collective Agreement",
+  "MTABA Collective Agreement",
+  "TRCLB Collective Agreement",
+];
 export default {
   components: {
     Header,
-    
+    Container,
+    Menu,
+    MenuButton,
+    MenuItems,
+    MenuItem,
+    DownloadIcon,
   },
-  
+  data() {
+    return {
+      options,
+      selected: "2020",
+      newsletters,
+    };
+  },
+  methods: {
+    setSelected(option) {
+      this.selected = option;
+    },
+  },
 };
 </script>
--- src/views/contact/index.vue
+++ src/views/contact/index.vue
@@ -1,20 +1,68 @@
 <template>
-  <Header title='contact' />
-  
-  
+  <Header title="contact us" />
+  <Container>
+    <div class="divide-y-2 divide-accent-dark">
+      <div class="grid gap-8 md:grid-cols-3 py-12 pt-0">
+        <div>
+          <h2 class="text-4xl">Contact Information</h2>
+        </div>
+        <div>
+          <h3 class="text-offblack text-xl capitalize mb-2">Mailing Address</h3>
+          <address class="not-italic">
+            25 North Rivermede Road, Unit 13 <br />
+            Vaughan, Ontario L4K 5V4
+          </address>
+          <h3 class="text-offblack text-xl capitalize mb-2 mt-6">Fax</h3>
+          <a class="text-accent underline" href="tel:+905-760-7718">905-760-7718</a>
+        </div>
+        <div>
+          <h3 class="text-offblack text-xl capitalize mb-2">Telephone</h3>
+          <div class="space-y-2">
+            <a class="block text-accent underline" href="tel:+905-760-7777">905-760-7777</a>
+            <a class="block text-accent underline" href="tel:+1-866-531-1608">1-866-531-1608</a>
+          </div>
+          <h3 class="text-offblack text-xl capitalize mb-2 mt-6">Social Media</h3>
+          <div class="flex items-center space-x-5">
+            <a href="#"><img src="@/assets/twitter.svg" alt="" /></a>
+            <a href="#"><img src="@/assets/linkedin.svg" alt="" /></a>
+          </div>
+        </div>
+      </div>
+      <div class="grid gap-8 md:grid-cols-3 py-12">
+        <div>
+          <h2 class="text-4xl">Membership</h2>
+        </div>
+        <div>
+          <h3 class="text-offblack text-xl capitalize mb-2">Membership Inquiries</h3>
+          <p>Andrew Pariser</p>
+          <a class="text-accent underline" href="mailto:pariser@rescon.com">pariser@rescon.com</a>
+        </div>
+      </div>
+      <div class="grid gap-8 md:grid-cols-3 py-12">
+        <div>
+          <h2 class="text-4xl">Media</h2>
+        </div>
+        <div>
+          <h3 class="text-offblack text-xl capitalize mb-2">Media Coverage</h3>
+          <p>Grant Cameron</p>
+          <a class="text-accent underline" href="mailto:cameron@rescon.com">cameron@rescon.com</a>
+        </div>
+      </div>
+    </div>
+  </Container>
+  <div class="md:h-1/5 h-80 w-full object-cover">
+    <img src="@/assets/images/map.png" alt="map of birmingham" class="w-full h-full object-cover" />
+  </div>
 </template>
 
 <script>
+import Container from "../../components/layouts/Container.vue";
 import Header from "@/components/base/Header";
-
-
-    
 
 export default {
   components: {
     Header,
-    
+    Container,
   },
-  
 };
 </script>
